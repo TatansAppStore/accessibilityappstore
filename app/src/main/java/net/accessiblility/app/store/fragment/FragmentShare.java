20ab6bf1b42ffd51a0d670bc00d65cb21d16c9f7
joakim_zhou <415908914@qq.com>
2017-01-05 11:25:08 +0800

    测试提交能否成功

--- app/src/main/java/net/accessiblility/app/store/fragment/FragmentShare.java
+++ app/src/main/java/net/accessiblility/app/store/fragment/FragmentShare.java
@@ -159,7 +159,6 @@ public class FragmentShare extends BaseFragment implements View.OnClickListener 
             @Override
             public void onSuccess(String s) {
                 super.onSuccess(s);
-                Log.d(TAG, "onSuccess: " + s);
             }
 
             @Override
